169c14d39be7adbc635333e37d7c7240ed429ef4
Rozaq <rozaqadi@baezeni.com>
2021-07-19 22:09:59 +0700

    latihan js

---
+++ 2021_07_19/itemku_nomor1.js
@@ -0,0 +1,28 @@
+const contohArr = [
+    "Enter uid1234 Muzi", 
+    "Enter uid4567 Prodo", 
+    "Leave uid1234", 
+    "Enter uid1234 Prodo", 
+    "Change uid4567 Ryan"
+]
+
+let user = {} // key = id, value = name
+let result = []
+
+contohArr.forEach(step => {
+    const [command, id, ...name] = step.split(' ');
+    if(command === "Enter") {
+        result.push(id + " came in")
+        user[id] = name
+    } else if(command === "Leave") {
+        result.push(id + " has left")
+    } else if(command === "Change") {
+        user[id] = name;
+    }
+})
+
+result = result.map(e => {
+    const [id, ...text] = e.split(' ')
+    return user[id] + " " + text
+})
+
