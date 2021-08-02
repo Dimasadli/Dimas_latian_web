531b4701dfa79302387592dd0d3eafe40f7fc604
Rozaq <rozaqadi@baezeni.com>
2021-08-03 00:19:16 +0700

    router

--- mini-chall-paragon/src/index.js
+++ mini-chall-paragon/src/index.js
@@ -8,7 +8,16 @@ import './font/Effra/Effra_Std_He.ttf'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Fragment>
+        <Route path='/tes' exact component={Home} />
+        {/* <Route path='/detail-post/:postID' component={DetailPost} />
+        <Route path='/product' component={Product} />
+        <Route path='/lifecycle' component={LifeCycle} />
+        <Route path='/youtube' component={YoutubeCompPage} />
+        <Route path='/tabel' component={Tabel} /> */}
+      </Fragment>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
