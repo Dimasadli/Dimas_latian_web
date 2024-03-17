e384ac3f77dbf23d241de2a00121eb06b7b8f7ae
Solution Delivery <solutiondeliveryparagon@gmail.com>
2024-03-18 01:26:30 +0700

    editing

--- 2022_01_18/longestSubstr.js
+++ 2022_01_18/longestSubstr.js
@@ -3,7 +3,6 @@ var lengthOfLongestSubstring = function (s) {
   let len = 0;
 
   for (let i = 0; i < s.length; i++) {
-    console.log(arr);
     if (arr.includes(s[i])) {
       if (len < arr.length) len = arr.length;
       let index = arr.indexOf(s[i]);
@@ -18,6 +17,28 @@ var lengthOfLongestSubstring = function (s) {
   } else {
     return len;
   }
+};
+
+var lengthOfLongestSubstring = function (str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      if (seen[char] > start) {
+        start = seen[char];
+      }
+    }
+    // index - beginning of substring + 1 (to include current in count)
+    if (i - start + 1 > longest) {
+      longest = i - start + 1;
+    }
+    // store the index of the next char so as to not double count
+    seen[char] = i + 1;
+  }
+  return longest;
 };
 
 lengthOfLongestSubstring("pwwkew");
